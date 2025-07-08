b059d0ad2457095fe805862e049bad3bbef152c9
speed <mp2943233@gmail.com>
2025-07-08 04:58:37 -0700

    Update ytmusic_server.py

--- ytmusic_server.py
+++ ytmusic_server.py
@@ -32,4 +32,6 @@ def search():
     return jsonify(mapped)
 
 if __name__ == '__main__':
-    app.run(port=5005) 
+    import os
+    port = int(os.environ.get('PORT', 5005))
+    app.run(host='0.0.0.0', port=port) 
